d2450e34280a50c6bafc8e30b681605a3e1c9484
Emirali Abduramanov <emiraliabduramanov@gmail.com>
2023-04-12 10:42:21 +0300

    Error handling: return json response with customized message

--- src/main/java/com/rgnrk/rgnrk_ti/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/rgnrk/rgnrk_ti/controller/RestResponseEntityExceptionHandler.java
@@ -1,32 +1,35 @@
 package com.rgnrk.rgnrk_ti.controller;
 
-import com.rgnrk.rgnrk_ti.exceptions.SessionNotFoundException;
-import com.rgnrk.rgnrk_ti.exceptions.UserStoryForbiddenToDeleteException;
-import com.rgnrk.rgnrk_ti.exceptions.UserStoryNotFoundException;
-import com.rgnrk.rgnrk_ti.exceptions.VotingIsClosedException;
-import org.springframework.http.HttpHeaders;
+import com.rgnrk.rgnrk_ti.exceptions.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = { SessionNotFoundException.class, UserStoryNotFoundException.class })
-    protected ResponseEntity<Object> handleNotFoundEntities(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    public ErrorResult handleNotFoundException(Exception ex, WebRequest request) {
+        return new ErrorResult(ex.getMessage());
     }
 
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(value = { UserStoryForbiddenToDeleteException.class})
-    protected ResponseEntity<Object> handleForbiddenAccess(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+    protected ErrorResult handleForbiddenAccess(Exception ex, WebRequest request) {
+        return new ErrorResult(ex.getMessage());
     }
 
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = { VotingIsClosedException.class})
-    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    protected ErrorResult handleBadRequest(Exception ex, WebRequest request) {
+        return new ErrorResult(ex.getMessage());
     }
 }
---
+++ src/main/java/com/rgnrk/rgnrk_ti/exceptions/ErrorResult.java
@@ -0,0 +1,10 @@
+package com.rgnrk.rgnrk_ti.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorResult {
+    private String message;
+}
